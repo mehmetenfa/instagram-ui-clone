f3c71ab90fed2225f4a0d553ab96cb9e588df872
qxan <mehmet.enfa@gmail.com>
2023-02-03 20:06:06 +0300

    tabs

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Screens from "./Screens";
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <SafeAreaView style={{flex: 1, marginTop: 35}}>
+      <NavigationContainer>
         <Screens />
-    </NavigationContainer>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
--- Screens.js
+++ Screens.js
@@ -1,15 +1,30 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
+
+// screens
+
+import HomeScreen from "./screens/Home";
+import ProfileScreen from "./screens/Profile";
+import ReelScreen from "./screens/Reel";
+import SearchScreen from "./screens/Search";
+import ShopScreen from "./screens/Shop";
 
 function Screens() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Settings" component={SettingsScreen} />
-        </Tab.Navigator>
-    )
-
+  return (
+    <Tab.Navigator
+        screenOptions={{
+            tabBarShowLabel: false,
+            headerShown: false
+        }}
+    >
+      <Tab.Screen name="home" component={HomeScreen} />
+      <Tab.Screen name="search" component={SearchScreen} />
+      <Tab.Screen name="reel" component={ReelScreen} />
+      <Tab.Screen name="shop" component={ShopScreen} />
+      <Tab.Screen name="profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
 }
 
 export default Screens;
