59e04476a7281ddd7e164b6ac0ebec1c43f5abef
qxan <mehmet.enfa@gmail.com>
2023-02-04 18:11:40 +0300

    touchableopacity

--- screens/home/components/Header.js
+++ screens/home/components/Header.js
@@ -1,20 +1,39 @@
-import { StyleSheet, View } from "react-native";
-import { Logo } from "../../../icons"
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { Logo, Heart, Plus, Chat } from "../../../icons";
 
 function Header() {
-    return (
-        <View style={styles.header}>
-            <Logo size={104} fill='#000' />
-        </View>
-    )
+  return (
+    <View style={styles.header}>
+      <Logo size={104} fill="#000" />
+      <View style={styles.actions}>
+        <TouchableOpacity activeOpacity="" style={styles.button}>
+          <Plus size={24} fill="#000" />
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity="0.6" style={styles.button}>
+          <Heart size={24} fill="#000" />
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity="0.6" style={styles.button}>
+          <Chat size={24} fill="#000" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 export default Header;
 
 const styles = StyleSheet.create({
-    header: {
-        height: 36,
-        flexDirection: 'row',
-        alignItems: 'center',
-    }
-})
+  header: {
+    height: 36,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  actions: {
+    flexDirection: "row",
+  },
+  button: {
+    marginLeft: 20,
+  },
+});
