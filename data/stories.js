abbb1c6aa6a7ea27f68b6e2fdb31c41a92955517
qxan <mehmet.enfa@gmail.com>
2023-02-05 18:04:37 +0300

    datas

---
+++ data/stories.js
@@ -0,0 +1,72 @@
+export default [
+    {
+        id: 1,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+    {
+        id: 2,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+    {
+        id: 3,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+    {
+        id: 4,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+    {
+        id: 5,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+    {
+        id: 6,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+    {
+        id: 7,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+    {
+        id: 8,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+    {
+        id: 9,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+    {
+        id: 10,
+        user: {
+            name: 'Mehmet Enfa',
+            avatar: 'https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg',
+        }
+    },
+]
---
+++ screens/home/components/Stories.js
@@ -0,0 +1,11 @@
+import { ScrollView, View, Text } from "react-native"
+
+function Stories() {
+    return (
+        <ScrollView>
+
+        </ScrollView>
+    )
+}
+
+export default Stories;
