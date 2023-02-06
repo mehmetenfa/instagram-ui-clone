7ad37ce41b7d8e8e4b49e1bf8b7bec4046734c45
qxan <mehmet.enfa@gmail.com>
2023-02-06 20:09:08 +0300

    data posts and shared post added

---
+++ components/shared/Post.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+function Post() {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+export default Post;
---
+++ data/posts.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    id: 1,
+    description: `Sekmeler arasi ui senkronizasyonu saglamak icin BroadcastChannel API kullanabileceğinizi biliyor musunuz? Prototurk.com'a gidin ve 2 sekme açıp koyu moda geçmeyi deneyin ya da giriş yapmayı deneyin her iki sekmeninde senkron şekilde çalışacağını göreceksiniz. Bunu nasıl yapacağınız ve sonucu gösteren görsel ve videoyu inceleyebilirsiniz :)`,
+    date: "2023-02-06 10:00:00",
+    likes: 100,
+    comments: 24,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+  {
+    id: 2,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+  {
+    id: 3,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+  {
+    id: 4,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+  {
+    id: 5,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+  {
+    id: 6,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+  {
+    id: 7,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+  {
+    id: 8,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+  {
+    id: 9,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+  {
+    id: 10,
+    user: {
+      name: "Mehmet Enfa",
+      avatar:
+        "https://pbs.twimg.com/profile_images/1610177519495385090/gkpozyxi_400x400.jpg",
+    },
+  },
+];
